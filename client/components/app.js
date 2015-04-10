5cf3d3d2a54374fce536b44b2ad621ec45a0015a
Christian Brandalise <christian.brandalise@gmail.com>
2015-04-10 11:37:01 -0500

    implements angular routes views and controllers

--- client/components/app.js
+++ client/components/app.js
@@ -12,31 +12,16 @@ angular.module("Treadstone" , ['Treadstone.home',
       controller  : 'homeController'
     })
     .when('/search/:city/:distance', {
-      templateUrl : 'routes/routesView.html',
-      controller : 'routes/routesController' 
+      templateUrl : 'components/Routes/routesView.html',
+      controller : 'routesController' 
     })
     .when('/route/:route_id', {
-      templateUrl: 'routeView.html',
+      templateUrl: 'components/Route/routeView.html',
       controller: 'routeController'
     })
     .when('/route/add', {
-      templateUrl: 'routeAddView',
+      templateUrl: 'components/routeAddView.html',
       controller: 'routeController'
     })
 })// END CONFIG
-<<<<<<< HEAD
 
-
-// angular.module('ui.bootstrap').controller('ButtonsCtrl', function ($scope) {
-//   $scope.singleModel = 1;
-
-//   $scope.radioModel = 'Middle';
-
-//   $scope.checkModel = {
-//     left: false,
-//     middle: true,
-//     right: false
-//   };
-// });
-=======
->>>>>>> c13143c04f05af0ba54b5ebfd42cd53e68ae5171
--- client/components/home/homeController.js
+++ client/components/home/homeController.js
@@ -1,6 +1,5 @@
-
 angular.module('Treadstone.home', [])
 
 .controller('homeController', function ($scope){
-
+	//TODO
 })
