aad5f5dff3d6419b85d7f494d19e9927fee99388
Christian Brandalise <christian.brandalise@gmail.com>
2015-04-10 11:54:30 -0500

    refactored routes controller

--- client/components/Routes/RoutesController.js
+++ client/components/Routes/RoutesController.js
@@ -1,6 +1,5 @@
 angular.module('Treadstone.routes', [])
 .controller('routesController', function ($scope, $routeParams){
-	console.log($routeParams.city, $routeParams.distance);
 	//TODO
 	$scope.city = $routeParams.city;
 	$scope.distance = $routeParams.distance;
