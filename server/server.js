d8a44ddeebf08b397d395b918046154c49ae67da
Tyler Lambe <tylambe@live.com>
2015-04-16 11:52:39 -0500

    Updated API, set up login error message

--- server/server.js
+++ server/server.js
@@ -6,6 +6,13 @@ var session = require('express-session')
 
 var app = express();
 
+var allowCrossDomain = function(req, res, next){
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Methods', 'GET, PUT, POST, DELETE');
+  res.header('Access-Control-Allow-Headers', 'Content-Type');
+  next();
+}
+
 app.use(bodyParser.json());
 app.use(express.static('./client'));
 
