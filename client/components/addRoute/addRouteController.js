f0cdd580b01d0c0ca0a2914b641fbe7c1128edd0
Christian Brandalise <christian.brandalise@gmail.com>
2015-04-17 09:45:17 -0500

    route map renders with dummy data

--- client/components/addRoute/addRouteController.js
+++ client/components/addRoute/addRouteController.js
@@ -5,6 +5,7 @@ angular.module('Treadstone.addRoute', [])
 	var directionsDisplay = new google.maps.DirectionsRenderer({draggable: true});
 	var directionsService = new google.maps.DirectionsService();
 	
+	//TODO This needs to be removed in the future;
 	renderMap("Austin, TX");
 	$scope.submit = function(){
 		geocoderFactory.createGeocoder($scope.location, function(results, status){
@@ -23,8 +24,9 @@ angular.module('Treadstone.addRoute', [])
 		var position = {coords: {latitude: dir.request.origin.k, longitude: dir.request.origin.D}}
 		dir.request.routeName = "" + $scope.name;
 		dir.request.routeDescription = "" + $scope.description;
-
 		getCity(position, function(cityState) {
+			console.log("Dir object:", dir.request);
+			console.log("JSON object", JSON.stringify(dir.request));
 			dir.request.cityState = cityState;
 		});
 
@@ -107,63 +109,5 @@ angular.module('Treadstone.addRoute', [])
 		return location;
 	}
 })
-
-// .directive('mapDirective', function() {
-
-// 	return function($scope) {
-
-// 		var rendererOptions = {
-// 		  draggable: true,
-// 		};
-
-// 		var directionsDisplay = new google.maps.DirectionsRenderer(rendererOptions);;
-// 		var directionsService = new google.maps.DirectionsService();
-// 		var map;
-
-// 		// var centerPoint = new google.maps.LatLng($scope.lat, $scope.lon);
-
-// 		  var mapOptions = {
-// 		    zoom: 8,
-// 		    center: $scope.center,
-// 		    disableDefaultUI: false
-// 		  };
-
-// 		  map = new google.maps.Map(document.getElementById('map-canvas'), mapOptions);
-// 		  directionsDisplay.setMap(map);
-// 		  directionsDisplay.setPanel(document.getElementById('directionsPanel'));
-
-// 		  google.maps.event.addListener(directionsDisplay, 'directions_changed', function() {
-// 		    computeTotalDistance(directionsDisplay.getDirections());
-// 		  });
-
-// 		  calcRoute();
-		
-
-// 		function calcRoute() {
-// 		  console.log("cityA", $scope.cityA, "cityB", $scope.cityB);
-// 		  var request = {
-// 		    origin: 'Austin, TX',
-// 		    destination: 'Dallas, TX',
-// 		    waypoints:[{location: 'San Antonio, TX'}],
-// 		    travelMode: google.maps.TravelMode.BICYCLING
-// 		  };
-// 		  directionsService.route(request, function(response, status) {
-// 		    if (status == google.maps.DirectionsStatus.OK) {
-// 		      directionsDisplay.setDirections(response);
-// 		    }
-// 		  });
-// 		}
-
-// 		function computeTotalDistance(result) {
-// 		  var total = 0;
-// 		  var myroute = result.routes[0];
-// 		  for (var i = 0; i < myroute.legs.length; i++) {
-// 		    total += myroute.legs[i].distance.value;
-// 		  }
-// 		  total = total / 1000.0;
-// 		  document.getElementById('total').innerHTML = total + ' km';
-// 		}
-// 	}
-// })
 
 
