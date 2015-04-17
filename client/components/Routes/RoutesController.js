3e8842019e2d883f99a6a2f9bf64d242c49dfef0
Jamie Skinner <skinner520@gmail.com>
2015-04-17 15:08:11 -0500

    Added comments

--- client/components/Routes/RoutesController.js
+++ client/components/Routes/RoutesController.js
@@ -1,6 +1,28 @@
 angular.module('Treadstone.routes', [])
-.controller('routesController', function ($scope, $routeParams){
+.controller('routesController', function ($scope, $routeParams, $http){
 	
 	$scope.city = $routeParams.city;
-	$scope.distance = $routeParams.distance;
+  $scope.routes = [];
+  $scope.supported;
+
+  $scope.init = function(){
+    $http({
+      method: "GET",
+      url: "/api/routes/" + $scope.city
+    }).then(function(data){
+      $scope.routes = data;
+      //You have to parse the data coming from the server
+      $scope.routes.map(function(item){
+        item = JSON.parse(item)
+      })
+    })
+  }
+  //Calls init to get routes. 
+  $scope.init();
+
+  if($scope.routes.hasOwnProperty("error")){
+
+  }
+  
+
 });
