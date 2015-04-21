c2fee831cc9718a973ed1a829fbefbe6386085d2
Jamie Skinner <skinner520@gmail.com>
2015-04-20 19:22:32 -0500

    Added elevation profile

--- client/components/Route/RouteController.js
+++ client/components/Route/RouteController.js
@@ -2,7 +2,8 @@ angular.module('Treadstone.route', [])
 
 .controller('routeController', function ($scope, $routeParams, $http, $location) {
 	$scope.route = {};
-
+	$scope.elevationClicked = false;
+	$scope.elevationCount = 0;
 
 	$scope.deleteRoute = function(){
 		if(confirm("Are you sure you want to delete this route? This will delete the route for everyone.")){			
@@ -18,6 +19,57 @@ angular.module('Treadstone.route', [])
 	$scope.hyperlapse = function(){		
 		$location.path('/route/hyperlapse/' + $routeParams.route_id);
 	}
+
+
+///ELEVATION CODE
+	$scope.getElevation= function(){
+		$scope.elevationClicked = !($scope.elevationClicked);
+		$scope.elevationCount+=1;
+			var req = $scope.route.path;
+			var path = [];
+
+			// google.load('visualization', '1.0', {'packages':['columnchart']});
+			var elev = new google.maps.ElevationService();
+
+			//creates the path
+			path.push(new google.maps.LatLng(req.origin.k,req.origin.D))
+	 		req.waypoints.forEach(function(wp){
+	 			path.push(new google.maps.LatLng(wp.location.k , wp.location.D))
+	 		});
+	 		path.push(new google.maps.LatLng(req.destination.k, req.destination.D));
+
+	    var pathRequest = {
+		    'path': path,
+		    'samples': 256
+	    }   
+
+	    elev.getElevationAlongPath(pathRequest, plotElevation);
+
+	    function plotElevation(results, status){
+	    	if(status != google.maps.ElevationStatus.OK){
+	    		return;
+	    	}
+	    	var data = new google.visualization.DataTable();
+	    	var elevations = results;
+    	  data.addColumn('string', 'Sample');
+    	  data.addColumn('number', 'Elevation');
+    	  for (var i = 0; i < results.length; i++) {
+    	    data.addRow(['', elevations[i].elevation]);
+    	  }
+
+    	  // Draw the chart using the data within its DIV.
+    	  document.getElementById('elevation_chart').style.display = 'block';
+    	  chart = new google.visualization.ColumnChart(document.getElementById('elevation_chart'));
+
+    	  chart.draw(data, {
+    	    height: 150,
+    	    legend: 'none',
+    	    titleY: 'Elevation (m)'
+    	  });
+      }
+	}// end getElevation
+
+	//END ELEVATION CODE
 
   function parseWaypoints(waypoints){
     var wpArray=[]
@@ -36,7 +88,11 @@ angular.module('Treadstone.route', [])
 		method: 'GET',
 		url: '/api/routes/'+ $routeParams.route_id,
 	}).then(function(route) {
+
     var routeInfo = JSON.parse(route.data.data);
+    console.log(routeInfo)
+    //path is the entire route object returned from server. 
+    $scope.route.path= routeInfo
     $scope.route.name = routeInfo.routeName;
     $scope.route.description = routeInfo.routeDescription;
 		renderMap(routeInfo.origin, parseWaypoints(routeInfo.waypoints), routeInfo.destination, routeInfo.travelMode);
